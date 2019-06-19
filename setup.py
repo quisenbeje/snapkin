1759893f365bf340bbf280c394fcffb9378f03ee
JJ Quisenberry <johnny.quisenberry@faa.gov>
2019-06-19 14:52:33 -0500

    reworks project and setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from glob import glob
+from os.path import basename
+from os.path import splitext
 
 setup(
     name='snapkin',
-    # version='0.1.0',
+    version='0.1.0',
+    description='btrfs snapshot clean up',
     author='JJ Quisenberry',
-    py_modules=['snapkin'],
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    author_email='johnny.e.quisenberry@gmail.com',
+    packages=find_packages('src/snapkin'),
+    package_dir={'': 'src/snapkin'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/snapkin/*.py')],
+    include_package_data=True,
+    zip_safe=False,
     install_requires=[
+        'bumpversion',
         'Click',
     ],
     entry_points='''
